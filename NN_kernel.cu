e65fa610294728c8154ecbc9ec066f902e4575b5
aniketdali <aniket.dali@gmail.com>
2018-04-28 21:48:20 -0700

    modified 2D convolution

--- NN_kernel.cu
+++ NN_kernel.cu
@@ -50,9 +50,9 @@ __global__ void executeFirstLayer(float *Layer1_Neurons_GPU,float *Layer1_Weight
 
 __constant__ int kernelTemplate2[25] = {
         0,  1,  2,  3,  4,
-        13, 14, 15, 16, 17, 
+        13, 14, 15, 16, 17,
         26, 27, 28, 29, 30,
-        39, 40, 41, 42, 43, 
+        39, 40, 41, 42, 43,
         52, 53, 54, 55, 56   };
 
 __global__ void executeSecondLayer(float *Layer2_Neurons_GPU, float *Layer2_Weights_GPU,float *Layer3_Neurons_GPU)
@@ -65,12 +65,12 @@ __global__ void executeSecondLayer(float *Layer2_Neurons_GPU, float *Layer2_Weig
 	int weightBegin=blockID*26*6;
 	int windowX=pixelX*2;
 	int windowY=pixelY*2;
-    
+
 	float result=0;
 
-	
+
 	result+=Layer2_Weights_GPU[weightBegin];
-	
+
 	if(blockID==1 && pixelX==0 && pixelY==0)
 	{
 		result+=0;
@@ -100,7 +100,7 @@ __global__ void executeThirdLayer(float *Layer3_Neurons_GPU, float *Layer3_Weigh
 
 
 	int weightBegin=blockID*1251;
- 
+
 	float result=0;
 
 	result+=Layer3_Weights_GPU[weightBegin];
@@ -125,7 +125,7 @@ __global__ void executeFourthLayer(float *Layer4_Neurons_GPU,float *Layer4_Weigh
 
 
 	int weightBegin=blockID*101;
- 
+
 	float result=0;
 
 	result+=Layer4_Weights_GPU[weightBegin];
@@ -142,32 +142,73 @@ __global__ void executeFourthLayer(float *Layer4_Neurons_GPU,float *Layer4_Weigh
 	Layer5_Neurons_GPU[blockID+(10*blockIdx.y)]=result;
 }
 
+
+__global__ void cnvolution2D(float *inputLayerNeurons, float *outputLayerNeurons,float *layerWeights, int part)
+{
+  int tx = threadIdx.x;
+  int ty = threadIdx.y;
+  int row_output = blockIdx.y* TILE_SIZE + ty;
+  int col_output = blockIdx.x* TILE_SIZE + tx;
+  int row_input  = row_output - MASK_WIDTH/2;
+  int row_input  = row_output - MASK_WIDTH/2;
+
+  float output =0.0f;
+  __shared_float Ns[TITLE_WIDTH + MASK_WIDTH -1] [TITLE_WIDTH + MASK_WIDTH -1];
+
+  if((row_input >=0)) && (row_input <N.height) &&
+     (col_input >=0) && (col_input <N.width))
+     {
+       Ns[ty][tx] = N.elements[row_input* N.width +col_input];
+     }
+     else
+     {
+       Ns[ty][tx] =0.0f;
+     }
+
+     if(ty <TITLE_WIDTH && tx <TITLE_WIDTH)
+     {
+       for(int i = 0; i< MASK_WIDTH;i++)
+       {
+         for(int j=0; j<MASK_WIDTH;j++)
+         {
+           output +=Mc[i][j] * Ns[i+ty][j=tx];
+         }
+       }
+     }
+
+     if(row_output<P.height && col_output <p.width)
+     {
+       p.elements[row_output * P.width + col_output] = output;
+     }
+
+}
+
 #endif // #ifndef _NN_KERNEL_H_
 
 
 
-__global__ void cnvolution3D(float *inputLayerNeurons, float *outputLayerNeurons,float *layerWeights, int part)
-{
-    float temp;
-    for(int channels=0;channels<layers[part+1][3]; channels++)
-	{
-		for(int sizeRow=0;sizeRow<layers[part+1][1]; sizeRow++)
-		{
-			for(int sizeCol=0;sizeCol<layers[part+1][2]; sizeCol++)
-			{
-				for(int k=0;k<layers[part][3]; k++)
-				{
-					for(int i=0;i<3;i++)
-					{
-						for(int i=0;i<3;i++)
-						{
-							//outputLayerNeurons[] += inputLayerNeurons[] * layerWeights[] ;
-							temp = += inputLayerNeurons[] * layerWeights[] ;
-						}
-					}
-				}
-			}
-		}
-	}
-    
-}
+// __global__ void cnvolution3D(float *inputLayerNeurons, float *outputLayerNeurons,float *layerWeights, int part)
+// {
+//     float temp;
+//     for(int channels=0;channels<layers[part+1][3]; channels++)
+// 	{
+// 		for(int sizeRow=0;sizeRow<layers[part+1][1]; sizeRow++)
+// 		{
+// 			for(int sizeCol=0;sizeCol<layers[part+1][2]; sizeCol++)
+// 			{
+// 				for(int k=0;k<layers[part][3]; k++)
+// 				{
+// 					for(int i=0;i<3;i++)
+// 					{
+// 						for(int i=0;i<3;i++)
+// 						{
+// 							//outputLayerNeurons[] += inputLayerNeurons[] * layerWeights[] ;
+// 							temp = += inputLayerNeurons[] * layerWeights[] ;
+// 						}
+// 					}
+// 				}
+// 			}
+// 		}
+// 	}
+//
+// }
